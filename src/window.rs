0cf38d6ef2d32c81c0e6fa768664f27c9983357b
Eric Rauer <eric@rauer.dev>
2023-02-04 13:23:33 -0800

    Use published gnome extension

--- src/window.rs
+++ src/window.rs
@@ -35,12 +35,12 @@ fn get_window_title_from_gnome() -> Result<String, Box<dyn std::error::Error>> {
     let conn = Connection::new_session()?;
     let proxy = conn.with_proxy(
         "org.gnome.Shell",
-        "/org/gnome/Shell/Extensions/Windows",
+        "/org/gnome/Shell/Extensions/WindowsExt",
         Duration::from_millis(5000),
     );
 
     let (title,): (String,) =
-        proxy.method_call("org.gnome.Shell.Extensions.Windows", "GetFocusedTitle", ())?;
+        proxy.method_call("org.gnome.Shell.Extensions.WindowsExt", "FocusTitle", ())?;
     return Ok(title);
 }
 

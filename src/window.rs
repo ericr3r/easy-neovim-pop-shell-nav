2079016f40efe80c859a2a813c3596d9dca1df3b
Eric Rauer <eric@rauer.dev>
2023-02-03 18:32:32 -0800

    Support parsing terminal title

--- src/window.rs
+++ src/window.rs
@@ -73,11 +73,29 @@ pub fn directory<'a>(window_name: &'a str) -> Option<&'a str> {
         return Some(directory);
     }
 
+    if let Some(directory) = directory_from_terminal_title(window_name) {
+        return Some(directory);
+    }
+
     None
 }
 
 fn directory_from_nvim_title<'a>(window_name: &'a str) -> Option<&'a str> {
     let caps = nvim_regex().captures(window_name)?;
+    let directory = caps.name("directory")?.as_str();
+    Some(directory)
+}
+
+fn directory_from_terminal_title<'a>(window_name: &'a str) -> Option<&'a str> {
+    let regex_string = beginning()
+        + one_or_more(any())
+        + text("@")
+        + one_or_more(any())
+        + text(":")
+        + named_capture(one_or_more(non_whitespace()), "directory")
+        + end();
+
+    let caps = regex_string.to_regex().captures(window_name)?;
     let directory = caps.name("directory")?.as_str();
     Some(directory)
 }

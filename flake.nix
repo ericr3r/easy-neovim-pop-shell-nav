25a726e6df57aa2552128e2424a6252e610456ff
Eric Rauer <eric@rauer.me>
2023-03-04 08:47:17 -0800

    Initial nix shell support

---
+++ flake.nix
@@ -0,0 +1,56 @@
+{
+  inputs = {
+    naersk.url = "github:nix-community/naersk/master";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+    utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = { self, nixpkgs, utils, naersk }:
+    utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = import nixpkgs { inherit system; };
+        naersk-lib = pkgs.callPackage naersk { };
+        libPath = with pkgs; lib.makeLibraryPath [
+          xdotool
+          xorg.libX11
+          dbus
+        ];
+      in
+      {
+        defaultPackage = naersk-lib.buildPackage { 
+          src = ./.;
+          doCheck = true;
+          pname = "easy-neovim-pop-shell-nav";
+          nativeBuildInputs = [ pkgs.makeWrapper ];
+          buildInputs = with pkgs; [
+            dbus
+            xdotool
+            xorg.libX11
+            pkgconfig
+          ];
+
+          postInstall = ''
+            wrapProgram "$out/bin/easy-neovim-pop-shell-nav" --prefix LD_LIBRARY_PATH : "${libPath}"
+          '';
+
+        };
+
+        apps.default = utils.lib.mkApp {
+          drv = self.defaultPackage."${system}";
+        };
+        devShell = with pkgs; mkShell {
+          buildInputs = [ 
+          cargo
+          rustc
+          rustfmt
+          pre-commit
+          rustPackages.clippy
+          pkgconfig
+          xdotool
+          xorg.libX11
+          dbus
+          ];
+          RUST_SRC_PATH = rustPlatform.rustLibSrc;
+        };
+      });
+}
